d9bd14eeefb47e581bfd44fb00fead791fa39da9
juancamilogonzalez93 <108770899+juancamilogonzalez93@users.noreply.github.com>
2022-11-02 22:03:21 -0500

    Repositorios Faltantes

---
+++ Modelos/Mesas.py
@@ -0,0 +1,4 @@
+from Modelos.AbstractModelo import AbstractModelo
+
+class Mesas(AbstractModelo):
+    pass
---
+++ Repositorios/CandidatosRepositorio.py
@@ -0,0 +1,5 @@
+from Repositorios.InterfaceRepositorio import InterfaceRepositorio
+from Modelos.Resultados import Resultados
+
+class CandidatosRepositorio(InterfaceRepositorio[Resultados]):
+    pass
